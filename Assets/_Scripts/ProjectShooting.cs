d3d78a32edb7deeae79083ad9ee0cbbad0661516
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-26 17:18:58 -0500

    added grenade launcher

--- Assets/_Scripts/ProjectShooting.cs
+++ Assets/_Scripts/ProjectShooting.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class ProjectShooting : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+	public Rigidbody Projectile;
+  public Transform FlashPoint;
+  public float ProjectileForce = 500.0f;
+  public float FireRate = 0.25f;
+
+
+
+  private float NextFireTime;
+
+
 	void Update () {
-	
+    if(Input.GetButtonDown("Fire2") && Time.time > NextFireTime) {
+      Rigidbody cloneRB = Instantiate(Projectile, FlashPoint.position, Quaternion.identity) as Rigidbody;
+      cloneRB.AddForce(FlashPoint.transform.forward * ProjectileForce);
+      NextFireTime = Time.time + FireRate;
+    }
 	}
 }
